2ebe123c97032d4563dc371dee9ef72f3a3ce911
Georgios Kopanidis <kopanidis@me.com>
2024-01-22 11:23:33 +0000

    Prepares code for deployment

--- hwblog/settings.py
+++ hwblog/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n%2uuyd3e4@y2ki7uuxa$(u5pmrjbw(k=od$f55-d^r&c@m+#y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
 ALLOWED_HOSTS = ["localhost", '.herokuapp.com']
 
 
