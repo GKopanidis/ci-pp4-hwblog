19c748575f0c8618f1de5dc644c28808f3304992
Georgios Kopanidis <kopanidis@me.com>
2024-01-22 13:02:16 +0000

    Add SECRET_KEY to env.py and settings.py file

--- hwblog/settings.py
+++ hwblog/settings.py
@@ -23,11 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n%2uuyd3e4@y2ki7uuxa$(u5pmrjbw(k=od$f55-d^r&c@m+#y'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = 'DEBUG' in os.environ
-DEBUG = False
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = ["localhost", '.herokuapp.com']
 
