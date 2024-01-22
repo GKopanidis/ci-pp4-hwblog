cca995237bf0d738be4ab38a38b57b8af5a87839
Georgios Kopanidis <kopanidis@me.com>
2024-01-22 16:20:20 +0000

    Add post_detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
-from . import views
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
