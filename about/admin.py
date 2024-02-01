fabf26a8dfb14be97fd748a0efff2194836ba989
Georgios Kopanidis <kopanidis@me.com>
2024-02-01 13:37:25 +0000

    Add mark_as_read action to CollaborateRequestAdmin

--- about/admin.py
+++ about/admin.py
@@ -10,11 +10,15 @@ class AboutAdmin(SummernoteModelAdmin):
     """
     summernote_fields = ('content',)
 
+def mark_as_read(modeladmin, request, queryset):
+    queryset.update(read=True)
+mark_as_read.short_description = "Mark selected requests as read"
 
 @admin.register(CollaborateRequest)
 class CollaborateRequestAdmin(admin.ModelAdmin):
     """
     Lists message and read fields for display in admin
     """
-    list_display = ('message', 'read',)
-    fields = ['name', 'email', 'phone', 'message']
+    list_display = ('name', 'email', 'phone', 'message', 'read',)
+    actions = [mark_as_read]
+    fields = ['name', 'email', 'phone', 'message', 'read']
